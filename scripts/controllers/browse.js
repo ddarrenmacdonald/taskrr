b1260d234868ffc85ca3ba7f4ae53b05d67f8a1f
drrnmcdnld <drrnmcdnld@gmail.com>
2015-04-08 12:39:32 -0300

    Completed Task#4

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,13 +1,15 @@
 'use strict';
 
-app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment) {
 
 	$scope.searchTask = '';
 	$scope.tasks = Task.all;
 
+	$scope.user = Auth.user;
 	$scope.signedIn = Auth.signedIn;
 	
 	$scope.listMode = true;
+
 
 	if($routeParams.taskId) {
 		var task = Task.getTask($routeParams.taskId).$asObject();
@@ -22,11 +24,27 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Task,
 			$scope.isTaskCreator = Task.isCreator;
 			$scope.isOpen = Task.isOpen;
 		}
+
+		$scope.comments = Comment.comments(task.$id);
 	};
 
+	// ------------ TASK ----------------
 	$scope.cancelTask = function(taskId) {
 		Task.cancelTask(taskId).then(function() {
 			toaster.pop('success', "This task is cancelled successfully.");
+		});
+	};
+
+	// ------------- COMMENT ------------
+	$scope.addComment = function() {
+		var comment = {
+			content: $scope.content,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Comment.addComment($scope.selectedTask.$id, comment).then(function() {
+			$scope.content = '';
 		});
 	};
 
